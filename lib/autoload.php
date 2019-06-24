e6b326745cb1ba66ef1ccfa094f7c2c6e85c1dfe
xshadowx <xshadowx@intersvyaz.net>
2019-06-24 15:38:54 +0500

    Вынес index.php в отдельную директорию

---
+++ lib/autoload.php
@@ -0,0 +1,11 @@
+<?php
+define('ROOT_DIR', '/var/www');
+
+spl_autoload_register(function($class) {
+    $file = ROOT_DIR.'/'.str_replace('\\', '/', $class).'.php';
+    if (file_exists($file)) {
+        require_once $file;
+    } else {
+        throw new Exception("Could not find file $file");
+    }
+});
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+mb_internal_encoding('UTF-8');
+header('Content-Type: text/html; charset=utf-8');
+include_once '../lib/autoload.php';
